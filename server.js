f65788fbe9581999c6a23f5ae5e2ea685d5d18dd
Kelly Thompson <ksthompson48@gmail.com>
2020-04-20 12:04:14 -0500

    streamline code

--- server.js
+++ server.js
@@ -6,17 +6,17 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true, });
 
 app.use(require("./routes/htmlRoutes.js"));
-// app.use(require("./routes/apiRoutes.js"));
+app.use(require("./routes/apiRoutes.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
