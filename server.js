051434c85fc94125d0bcdc78a0eede31c75941ba
Kelly Thompson <ksthompson48@gmail.com>
2020-04-20 12:35:41 -0500

    server ready for heroku

--- server.js
+++ server.js
@@ -13,7 +13,11 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true, });
+mongoose.connect(process.env.MONGODB_URI || 
+  "mongodb://kelly:kelly1@ds337418.mlab.com:37418/heroku_b8bz58zf",
+  { 
+    useNewUrlParser: true
+  });
 
 app.use(require("./routes/htmlRoutes.js"));
 app.use(require("./routes/apiRoutes.js"));
